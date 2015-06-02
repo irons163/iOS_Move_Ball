d0bf8c6ce46f836fd584ed12f059de2c5e9a7287
irons163 <irons@ironsdeMacBook-Air.local>
2015-06-02 17:31:22 +0800

    Initial Commit

---
+++ Try_move_ball/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Try_move_ball
+//
+
+//  Copyright (c) 2015年 irons. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Try_move_ball/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Try_move_ball
+//
+
+//  Copyright (c) 2015年 irons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
